fae1ad4ec0bba810444169f9e8336166a94e38e7
Karthik RP <blendedidea@gmail.com>
2021-08-01 18:06:59 +0530

    Restaurant detail page.

--- src/Restaurants/RestaurantHeader/RestaurantHeader.js
+++ src/Restaurants/RestaurantHeader/RestaurantHeader.js
@@ -3,7 +3,9 @@ import Button from '../../components/Button/Button';
 import { ReactComponent as BackIcon } from '../../resources/icons/chevron-left.svg';
 import { ReactComponent as FilterIcon } from '../../resources/icons/filter.svg';
 import { ReactComponent as SearchIcon } from '../../resources/icons/search.svg';
+import { ReactComponent as RestaurantIcon } from '../../resources/icons/restaurant.svg';
 import { ReactComponent as CartIcon } from '../../resources/icons/shopping-cart.svg';
+import { ReactComponent as MenuIcon } from '../../resources/icons/menu.svg';
 import './RestaurantHeader.scss';
 
 const RestaurantHeader = () => {
@@ -14,6 +16,12 @@ const RestaurantHeader = () => {
       <Button iconOnly className="restaurant-header__back-icon">
         <BackIcon />
       </Button>
+
+      <div className="restaurant-header__location">
+        <RestaurantIcon className="restaurant-header__location__restaurant-icon" />
+        <h5 className="restaurant-header__location__name">Da Otto</h5>
+        <MenuIcon className="restaurant-header__location__menu-icon" />
+      </div>
 
       <div className="restaurant-header__search">
         <SearchIcon className="restaurant-header__search-icon" />
